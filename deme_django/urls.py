137acadbb2e5483225704343c4143121e535160c
Mike Mintz <mikemintz@cs.stanford.edu>
2008-08-11 08:06:35 +0000

    Added an option for whether Django or default (Apache usually) handles static files

--- deme_django/urls.py
+++ deme_django/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls.defaults import *
-import os.path
+from django.conf import settings
 
-urlpatterns = patterns('',
-    # Comment this out for production:
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.dirname(__file__), 'static')}),
+urlpatterns = patterns('')
+
+if settings.DJANGO_SERVES_STATIC_FILES:
+    import os.path
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.dirname(__file__), 'static')}),
+    )
+
+urlpatterns += patterns('',
     # Uncomment this for admin:
     # (r'^admin/', include('django.contrib.admin.urls')),
     (r'', include('deme_django.cms.urls')),
 )
+
