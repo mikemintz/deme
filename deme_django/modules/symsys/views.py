6a65cac74303f20e6f9c4c84c7678755e17d3947
Mike Mintz <mikemintz@cs.stanford.edu>
2008-10-12 08:04:53 +0000

    Fixed a place where I forgot to update permission code

--- deme_django/modules/symsys/views.py
+++ deme_django/modules/symsys/views.py
@@ -10,11 +10,6 @@ class SymsysAffiliateViewer(ItemViewer):
     viewer_name = 'symsysaffiliate'
 
     def entry_show(self):
-        can_do_everything = ('do_everything', 'Item') in permission_functions.get_global_abilities_for_agent(self.cur_agent)
-        abilities_for_item = permission_functions.get_abilities_for_agent_and_item(self.cur_agent, self.item)
-        can_view = ('view', 'id') in abilities_for_item
-        if not (can_do_everything or can_view):
-            return HttpResponseBadRequest("you do not have permission to view this item")
         template = loader.get_template('symsysaffiliate/show.html')
         return HttpResponse(template.render(self.context))
 
