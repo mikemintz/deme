ea7d90afb4113d87fa2103f71244a7a0e58a46e0
Mike Mintz <mikemintz@cs.stanford.edu>
2008-12-18 13:41:24 -0900

    Renamed SymsysAffiliate graduation_year to bs_graduation_year and ms_graduation to ms_graduation_year.

--- deme_django/modules/symsys/models.py
+++ deme_django/modules/symsys/models.py
@@ -11,7 +11,7 @@ class SymsysAffiliate(Person):
 
     class_year = models.IntegerField(null=True, blank=True)
     first_affiliation_year = models.IntegerField(null=True, blank=True)
-    graduation_year = models.IntegerField(null=True, blank=True)
+    bs_graduation_year = models.IntegerField(null=True, blank=True)
 
     academic_title = models.CharField(max_length=255, blank=True, choices=[("Assistant Professor", "Assistant Professor"), ("Associate Professor", "Associate Professor"), ("Associate Professor (Research)", "Associate Professor (Research)"), ("Consulting Assistant Professor", "Consulting Assistant Professor"), ("Consulting Associate Professor", "Consulting Associate Professor"), ("Consulting Professor", "Consulting Professor"), ("Engineering Research Associate", "Engineering Research Associate"), ("Executive Director", "Executive Director"), ("Lecturer", "Lecturer"), ("Professor", "Professor"), ("Professor Emeritus", "Professor Emeritus"), ("Senior Lecturer", "Senior Lecturer"), ("Senior Research Engineer", "Senior Research Engineer"), ("Student Services Officer", "Student Services Officer"), ("University Affiliate", "University Affiliate"), ("Wasow Visiting Lecturer", "Wasow Visiting Lecturer"), ("Web Design", "Web Design"),])
     admin_title = models.CharField(max_length=255, blank=True, choices=[("Advising Fellow", "Advising Fellow"), ("AF", "AF"), ("Associate Director", "Associate Director"), ("Director Emeritus", "Director Emeritus"), ("Graduate Studies Director", "Graduate Studies Director"), ("Program Director", "Program Director"), ("Student Services Officer", "Student Services Officer"), ("Student Services Officer (on leave)", "Student Services Officer (on leave)"), ("Web Developer", "Web Developer"), ("Webmaster", "Webmaster"), ("Webmaster (2003-2004)", "Webmaster (2003-2004)"),])
@@ -59,7 +59,7 @@ class SymsysAffiliate(Person):
     indivdesignedconc = models.CharField(max_length=255, blank=True)
     department2 = models.CharField(max_length=255, blank=True)
     ms_track = models.CharField(max_length=255, blank=True)
-    ms_graduation = models.IntegerField(blank=True, null=True)
+    ms_graduation_year = models.IntegerField(blank=True, null=True)
 
     honors_thesis_title = models.CharField(max_length=255, blank=True)
     honors_thesis_filename = models.CharField(max_length=255, blank=True)
