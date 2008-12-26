30f589d47eafefc24b2e0f24ea9c0080b599f20a
Mike Mintz <mikemintz@cs.stanford.edu>
2008-12-25 21:01:01 -0900

    Fixed broken set_default_layout

--- deme_django/cms/views.py
+++ deme_django/cms/views.py
@@ -131,6 +131,36 @@ class ViewerMetaClass(type):
         ViewerMetaClass.viewer_name_dict[attrs['viewer_name']] = result
         return result
 
+def set_default_layout(context, site, cur_agent):
+    can_do_everything = ('do_everything', 'Item') in permission_functions.get_global_abilities_for_agent(context['cur_agent'])
+    cur_node = site.default_layout
+    while cur_node is not None:
+        next_node = cur_node.layout
+        if next_node is None:
+            if cur_node.override_default_layout:
+                extends_string = ''
+            else:
+                extends_string = "{% extends 'default_layout.html' %}\n"
+        else:
+            extends_string = "{%% extends layout%s %%}\n" % next_node.pk
+        if can_do_everything or (('view', 'layout') in permission_functions.get_abilities_for_agent_and_item(context['cur_agent'], cur_node) and ('view', 'body') in permission_functions.get_abilities_for_agent_and_item(context['cur_agent'], cur_node)):
+            template_string = extends_string + cur_node.body
+        else:
+            template_string = "{% extends 'default_layout.html' %}\n"
+            context['layout_permissions_problem'] = True
+            next_node = None
+        t = loader.get_template_from_string(template_string)
+        context['layout%d' % cur_node.pk] = t
+        cur_node = next_node
+    if can_do_everything or ('view', 'default_layout') in permission_functions.get_abilities_for_agent_and_item(context['cur_agent'], site):
+        if site.default_layout:
+            context['layout'] = context['layout%s' % site.default_layout.pk]
+        else:
+            context['layout'] = 'default_layout.html'
+    else:
+        context['layout'] = 'default_layout.html'
+        context['layout_permissions_problem'] = True
+
 def get_viewer_class_for_viewer_name(viewer_name):
     return ViewerMetaClass.viewer_name_dict.get(viewer_name, None)
 
@@ -190,36 +220,6 @@ class ItemViewer(object):
             self._item_ability_cache[(agent.pk, item.pk)] = result
         return result
 
-    def set_default_layout(self, site, cur_agent):
-        can_do_everything = ('do_everything', 'Item') in self.get_global_abilities_for_agent(self.cur_agent)
-        cur_node = site.default_layout
-        while cur_node is not None:
-            next_node = cur_node.layout
-            if next_node is None:
-                if cur_node.override_default_layout:
-                    extends_string = ''
-                else:
-                    extends_string = "{% extends 'default_layout.html' %}\n"
-            else:
-                extends_string = "{%% extends layout%s %%}\n" % next_node.pk
-            if can_do_everything or (('view', 'layout') in self.get_abilities_for_agent_and_item(self.cur_agent, cur_node) and ('view', 'body') in self.get_abilities_for_agent_and_item(self.cur_agent, cur_node)):
-                template_string = extends_string + cur_node.body
-            else:
-                template_string = "{% extends 'default_layout.html' %}\n"
-                self.context['layout_permissions_problem'] = True
-                next_node = None
-            t = loader.get_template_from_string(template_string)
-            self.context['layout%d' % cur_node.pk] = t
-            cur_node = next_node
-        if can_do_everything or ('view', 'default_layout') in self.get_abilities_for_agent_and_item(self.cur_agent, site):
-            if site.default_layout:
-                self.context['layout'] = self.context['layout%s' % site.default_layout.pk]
-            else:
-                self.context['layout'] = 'default_layout.html'
-        else:
-            self.context['layout'] = 'default_layout.html'
-            self.context['layout_permissions_problem'] = True
-
     def render_error(self, request_class, title, body):
         template = loader.get_template_from_string("""
         {%% extends layout %%}
@@ -267,7 +267,7 @@ class ItemViewer(object):
         self.context['cur_agent'] = self.cur_agent
         self.context['_global_ability_cache'] = self._global_ability_cache
         self.context['_item_ability_cache'] = self._item_ability_cache
-        self.set_default_layout(current_site, cur_agent)
+        set_default_layout(self.context, current_site, cur_agent)
 
     def init_from_div(self, original_request, action, viewer_name, item, cur_agent):
         self.request = original_request
