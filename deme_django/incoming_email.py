724fd856fc468821db62f6ed777e10c7f6527853
Mike Mintz <mikemintz@cs.stanford.edu>
2008-12-26 22:11:39 -0900

    Fixed incoming mails so they work with the new EmailContactMethod setup

--- deme_django/incoming_email.py
+++ deme_django/incoming_email.py
@@ -25,7 +25,7 @@ def main():
     to_email = email.utils.parseaddr(msg['To'])[1]
     item_id = to_email.split('@')[0]
     try:
-        person = cms.models.Person.objects.get(email=from_email)
+        email_contact_method = cms.models.EmailContactMethod.objects.get(email=from_email)
     except ObjectDoesNotExist:
         send_mail('Re: %s' % subject, 'Error: Deme could not create your comment "%s" because there does not exist a user with email address %s' % (subject, from_email), to_email, [from_email])
         return 
@@ -35,9 +35,9 @@ def main():
         send_mail('Re: %s' % subject, 'Error: Deme could not create your comment "%s" because there does not exist an item with id %s' % (subject, item_id), to_email, [from_email])
         return 
     comment = cms.models.TextComment(commented_item=item, name=subject, body=body)
-    comment.save_versioned(updater=person)
+    comment.save_versioned(updater=email_contact_method.agent)
     comment_location = cms.models.CommentLocation(name="Untitled CommentLocation", comment=comment, commented_item_version_number=item.versions.latest().version_number, commented_item_index=None)
-    comment_location.save_versioned(updater=person)
+    comment_location.save_versioned(updater=email_contact_method.agent)
 
 
 if __name__ == '__main__':
