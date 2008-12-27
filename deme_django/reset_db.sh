4d55a9dff974fdcfa5c96b681744b32017201c80
Mike Mintz <mikemintz@cs.stanford.edu>
2008-12-27 01:19:42 -0900

    We now create a postgres database with UTF-8 encoding

--- deme_django/reset_db.sh
+++ deme_django/reset_db.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -rf static/media/*
-psql -U postgres -c "drop database deme_django;" && psql -U postgres -c "create database deme_django;" && ./manage.py syncdb && ./create_initial_data.py test
+psql -U postgres -c "drop database deme_django;" ; psql -U postgres -c "create database deme_django WITH OWNER = postgres ENCODING = 'UTF8' TABLESPACE = pg_default;" && ./manage.py syncdb && ./create_initial_data.py test
