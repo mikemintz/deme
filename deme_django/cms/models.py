a8efa7a7ca6bd1ed28b239fb8f33e7567a119199
Mike Mintz <mikemintz@cs.stanford.edu>
2008-12-30 04:14:40 -0900

    Extracted IsDefaultField to a new cms/fields.py file.

--- deme_django/cms/models.py
+++ deme_django/cms/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.db.models import Q
-from django import forms
-from django.utils.translation import ugettext_lazy as _
 from django.db import transaction
 from django.db import connection
 import datetime
@@ -11,41 +9,11 @@ from django.core.mail import SMTPConnection, EmailMessage
 from email.utils import formataddr
 from django.core.urlresolvers import reverse
 import settings
+from fields import IsDefaultField
 
 from django.db.models.base import ModelBase
 from copy import deepcopy
 import hashlib
-
-
-class IsDefaultFormField(forms.BooleanField):
-    widget = forms.CheckboxInput
-    def clean(self, value):
-        if value == 'False':
-            return None
-        if value:
-            return True
-        return None
-
-
-class IsDefaultField(models.NullBooleanField):
-    def __init__(self, *args, **kwargs):
-        kwargs['unique'] = True
-        models.NullBooleanField.__init__(self, *args, **kwargs)
-
-    def get_internal_type(self):
-        return "NullBooleanField"
-
-    def to_python(self, value):
-        if value in (None, True): return value
-        if value in (False,): return None
-        if value in ('None'): return None
-        if value in ('t', 'True', '1'): return True
-        raise validators.ValidationError, _("This value must be either None or True.")
-
-    def formfield(self, **kwargs):
-        defaults = {'form_class': IsDefaultFormField}
-        defaults.update(kwargs)
-        return super(IsDefaultField, self).formfield(**defaults)
 
 
 class ItemMetaClass(ModelBase):
