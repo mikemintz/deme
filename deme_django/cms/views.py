35b34f482e646d74cdf4fbbf1d18b789fdb085f8
Mike Mintz <mikemintz@cs.stanford.edu>
2009-01-01 18:15:19 -0900

    Improved performance of ItemSetViewer.show by filtering by permission to view name in one query instead of n queries.

--- deme_django/cms/views.py
+++ deme_django/cms/views.py
@@ -949,7 +949,9 @@ class ItemSetViewer(ItemViewer):
         memberships = memberships.filter(item__trashed=False)
         memberships = memberships.filter(permission_functions.filter_for_agent_and_ability(self.cur_agent, 'view item'))
         memberships = memberships.filter(permission_functions.filter_for_agent_and_ability(self.cur_agent, 'view itemset'))
-        self.context['memberships'] = memberships
+        memberships = memberships.select_related('item')
+        memberships_can_view_name_pks = set(memberships.filter(item__pk__in=cms.models.Item.objects.filter(permission_functions.filter_for_agent_and_ability(self.cur_agent, 'view name')).values('pk').query).values_list('pk', flat=True))
+        self.context['memberships'] = [{'membership': x, 'can_view_name': x.pk in memberships_can_view_name_pks} for x in memberships]
         self.context['cur_agent_in_itemset'] = bool(self.item.memberships_as_itemset.filter(trashed=False, item=self.cur_agent))
         self.context['addmember_form'] = NewMembershipForm()
         template = loader.get_template('itemset/show.html')
