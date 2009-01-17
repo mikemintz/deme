aaeaeba02bf72bd06e9ace771fc6ed8e1d7f7e78
Mike Mintz <mikemintz@cs.stanford.edu>
2009-01-16 20:33:26 -0800

    Fixed incoming email script

--- deme_django/script/incoming_email.py
+++ deme_django/script/incoming_email.py
@@ -32,19 +32,19 @@ def main():
     to_email = email.utils.parseaddr(msg['To'])[1]
     item_id = to_email.split('@')[0]
     try:
-        email_contact_method = models.EmailContactMethod.objects.get(email=from_email)
+        email_contact_method = EmailContactMethod.objects.get(email=from_email)
     except ObjectDoesNotExist:
         send_mail('Re: %s' % subject, 'Error: Deme could not create your comment "%s" because there does not exist a user with email address %s' % (subject, from_email), to_email, [from_email])
         return 
     try:
-        item = models.Item.objects.get(pk=item_id)
+        item = Item.objects.get(pk=item_id)
     except ObjectDoesNotExist:
         send_mail('Re: %s' % subject, 'Error: Deme could not create your comment "%s" because there does not exist an item with id %s' % (subject, item_id), to_email, [from_email])
         return 
     if not permission_cache.agent_can(email_contact_method.agent, 'comment_on', item):
         send_mail('Re: %s' % subject, 'Error: Deme could not create your comment "%s" because you do not have permission to comment on the item with id %s' % (subject, item_id), to_email, [from_email])
         return 
-    comment = models.TextComment(item=item, item_version_number=item.version_number, name=subject, body=body)
+    comment = TextComment(item=item, item_version_number=item.version_number, name=subject, body=body)
     comment.save_versioned(updater=email_contact_method.agent)
 
 
