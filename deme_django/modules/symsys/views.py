f959e28a70cfa7893297c29e3f07e55b167477db
Mike Mintz <mikemintz@cs.stanford.edu>
2009-01-27 00:44:40 -0800

    Fixed symsysaffiliate viewer to take into account the new permissions api

--- deme_django/modules/symsys/views.py
+++ deme_django/modules/symsys/views.py
@@ -15,9 +15,9 @@ class SymsysAffiliateViewer(ItemViewer):
         if self.cur_agent_can_global('do_everything'):
             recursive_filter = None
         else:
-            visible_memberships = Membership.objects.filter(self.permission_cache.filter_items(self.cur_agent, 'view item'))
+            visible_memberships = self.permission_cache.filter_items(self.cur_agent, 'view item', Membership.objects)
             recursive_filter = Q(child_memberships__in=visible_memberships.values('pk').query)
         self.context['containing_collections'] = self.item.ancestor_collections(recursive_filter)
-        self.context['contact_methods'] = self.item.contact_methods.filter(self.permission_cache.filter_items(self.cur_agent, 'view agent'), trashed=False)
+        self.context['contact_methods'] = self.permission_cache.filter_items(self.cur_agent, 'view agent', self.item.contact_methods).filter(trashed=False)
         return HttpResponse(template.render(self.context))
 
