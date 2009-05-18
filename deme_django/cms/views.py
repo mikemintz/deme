be885a87785fe13de0f5ca16c68d6b7772c7d2ff
Mike Mintz <mikemintz@cs.stanford.edu>
2009-05-17 21:37:55 -0700

    Extracted openid to its own viewer.

--- deme_django/cms/views.py
+++ deme_django/cms/views.py
@@ -732,43 +732,6 @@ class AuthenticationMethodViewer(ItemViewer):
                     response_data = {'nonce':nonce, 'algo':'sha1', 'salt':'x'}
                 json_data = simplejson.dumps(response_data, separators=(',',':'))
                 return HttpResponse(json_data, mimetype='application/json')
-            # If openidcomplete is a key in the query string, the user has just
-            # authenticated with OpenID
-            elif 'openidcomplete' in self.request.GET:
-                redirect = self.request.GET['redirect']
-                try:
-                    import openid.consumer.consumer
-                except ImportError:
-                    return self.render_error(HttpResponseBadRequest, "Authentication Failed", "OpenID is not supported in this installation")
-                consumer = openid.consumer.consumer.Consumer(self.request.session, None)
-                query_dict = dict((k,v) for k,v in self.request.GET.items())
-                current_url = self.request.build_absolute_uri().split('?')[0]
-                openid_response = consumer.complete(query_dict, current_url)
-                
-                if openid_response.status == openid.consumer.consumer.SUCCESS:
-                    identity_url = openid_response.identity_url
-                    identity_url_without_fragment = identity_url.split('#')[0]
-                    # If we want to use display_identifier, we need to have python-openid >=2.1, which isn't in Ubuntu Hardy
-                    #display_identifier = openid_response.getDisplayIdentifier()
-                    sreg = openid_response.extensionResponse('sreg', False)
-                    try:
-                        openid_authentication_method = OpenidAccount.objects.get(Q(openid_url=identity_url_without_fragment) | Q(openid_url__startswith=identity_url_without_fragment + '#'))
-                    except ObjectDoesNotExist:
-                        # No OpenidAccount has this openid_url.
-                        return self.render_error(HttpResponseBadRequest, "Authentication Failed", "There is no active agent with that OpenID (1)")
-                    if not openid_authentication_method.active or not openid_authentication_method.agent.active: 
-                        # The Agent or OpenidAccount is inactive.
-                        return self.render_error(HttpResponseBadRequest, "Authentication Failed", "There is no active agent with that OpenID (2)")
-                    self.request.session['cur_agent_id'] = openid_authentication_method.agent.pk
-                    return HttpResponseRedirect(redirect)
-                elif openid_response.status == openid.consumer.consumer.CANCEL:
-                    return self.render_error(HttpResponseBadRequest, "Authentication Failed", "OpenID request was cancelled")
-                elif openid_response.status == openid.consumer.consumer.FAILURE:
-                    return self.render_error(HttpResponseBadRequest, "Authentication Failed", "OpenID error: %s" % escape(openid_response.message))
-                elif openid_response.status == openid.consumer.consumer.SETUP_NEEDED:
-                    return self.render_error(HttpResponseBadRequest, "Authentication Failed", "OpenID setup needed")
-                else:
-                    return self.render_error(HttpResponseBadRequest, "Authentication Failed", "Invalid OpenID status: %s" % escape(openid_response.status))
             # Otherwise, return the login.html page.
             else:
                 login_as_agents = Agent.objects.filter(active=True).order_by('name')
@@ -823,21 +786,6 @@ class AuthenticationMethodViewer(ItemViewer):
                         else:
                             # The current agent does not have permission to login_as the specified agent.
                             return self.render_error(HttpResponseBadRequest, "Authentication Failed", "There was a problem with your login form")
-            elif login_type == 'openid':
-                try:
-                    import openid.consumer.consumer
-                except ImportError:
-                    return self.render_error(HttpResponseBadRequest, "Authentication Failed", "OpenID is not supported in this installation")
-                trust_root = self.request.build_absolute_uri('/')
-                full_redirect = self.request.build_absolute_uri('%s?openidcomplete=1&redirect=%s' % (reverse('item_type_url', kwargs={'viewer': self.viewer_name, 'action': 'login'}), urlquote(redirect)))
-                user_url = self.request.POST['openid_url']
-                consumer = openid.consumer.consumer.Consumer(self.request.session, None)
-                try:
-                    auth_request = consumer.begin(user_url)
-                except openid.consumer.consumer.DiscoveryFailure:
-                    return self.render_error(HttpResponseBadRequest, "Authentication Failed", "Invalid OpenID URL")
-                auth_request.addExtensionArg('sreg', 'optional', 'nickname,email,fullname')
-                return HttpResponseRedirect(auth_request.redirectURL(trust_root, full_redirect))
             # Invalid login_type parameter.
             return self.render_error(HttpResponseBadRequest, "Authentication Failed", "There was a problem with your login form")
    
@@ -846,12 +794,70 @@ class AuthenticationMethodViewer(ItemViewer):
         redirect = self.request.GET['redirect']
         if 'cur_agent_id' in self.request.session:
             del self.request.session['cur_agent_id']
-        self.context["redirect"] = redirect
+        self.context['redirect'] = redirect
         template = loader.get_template('authenticationmethod/logout.html')
         return HttpResponse(template.render(self.context))
  
 
-class WebauthAccountViewer(ItemViewer):
+class OpenidAccountViewer(AuthenticationMethodViewer):
+    accepted_item_type = OpenidAccount
+    viewer_name = 'openidaccount'
+
+    def type_login_html(self):
+        redirect = self.request.GET['redirect']
+        try:
+            import openid.consumer.consumer
+        except ImportError:
+            return self.render_error(HttpResponseBadRequest, "Authentication Failed", "OpenID is not supported in this installation")
+        trust_root = self.request.build_absolute_uri('/')
+        full_redirect = self.request.build_absolute_uri('%s?redirect=%s' % (reverse('item_type_url', kwargs={'viewer': self.viewer_name, 'action': 'logincomplete'}), urlquote(redirect)))
+        user_url = self.request.POST['openid_url']
+        consumer = openid.consumer.consumer.Consumer(self.request.session, None)
+        try:
+            auth_request = consumer.begin(user_url)
+        except openid.consumer.consumer.DiscoveryFailure:
+            return self.render_error(HttpResponseBadRequest, "Authentication Failed", "Invalid OpenID URL")
+        auth_request.addExtensionArg('sreg', 'optional', 'nickname,email,fullname')
+        return HttpResponseRedirect(auth_request.redirectURL(trust_root, full_redirect))
+
+    def type_logincomplete_html(self):
+        redirect = self.request.GET['redirect']
+        try:
+            import openid.consumer.consumer
+        except ImportError:
+            return self.render_error(HttpResponseBadRequest, "Authentication Failed", "OpenID is not supported in this installation")
+        consumer = openid.consumer.consumer.Consumer(self.request.session, None)
+        query_dict = dict((k,v) for k,v in self.request.GET.items())
+        current_url = self.request.build_absolute_uri().split('?')[0]
+        openid_response = consumer.complete(query_dict, current_url)
+        
+        if openid_response.status == openid.consumer.consumer.SUCCESS:
+            identity_url = openid_response.identity_url
+            identity_url_without_fragment = identity_url.split('#')[0]
+            # If we want to use display_identifier, we need to have python-openid >=2.1, which isn't in Ubuntu Hardy
+            #display_identifier = openid_response.getDisplayIdentifier()
+            sreg = openid_response.extensionResponse('sreg', False)
+            try:
+                openid_authentication_method = OpenidAccount.objects.get(Q(openid_url=identity_url_without_fragment) | Q(openid_url__startswith=identity_url_without_fragment + '#'))
+            except ObjectDoesNotExist:
+                # No OpenidAccount has this openid_url.
+                return self.render_error(HttpResponseBadRequest, "Authentication Failed", "There is no active agent with that OpenID (1)")
+            if not openid_authentication_method.active or not openid_authentication_method.agent.active: 
+                # The Agent or OpenidAccount is inactive.
+                return self.render_error(HttpResponseBadRequest, "Authentication Failed", "There is no active agent with that OpenID (2)")
+            self.request.session['cur_agent_id'] = openid_authentication_method.agent.pk
+            return HttpResponseRedirect(redirect)
+        elif openid_response.status == openid.consumer.consumer.CANCEL:
+            return self.render_error(HttpResponseBadRequest, "Authentication Failed", "OpenID request was cancelled")
+        elif openid_response.status == openid.consumer.consumer.FAILURE:
+            return self.render_error(HttpResponseBadRequest, "Authentication Failed", "OpenID error: %s" % escape(openid_response.message))
+        elif openid_response.status == openid.consumer.consumer.SETUP_NEEDED:
+            return self.render_error(HttpResponseBadRequest, "Authentication Failed", "OpenID setup needed")
+        else:
+            return self.render_error(HttpResponseBadRequest, "Authentication Failed", "Invalid OpenID status: %s" % escape(openid_response.status))
+
+
+class WebauthAccountViewer(AuthenticationMethodViewer):
     accepted_item_type = WebauthAccount
     viewer_name = 'webauth'
 
