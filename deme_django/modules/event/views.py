d26b9bf52909245fae92b628178f188ba3b2a8f8
Joseph <joe@joe-laptop.(none)>
2009-07-03 18:33:21 -0700

    Made the calendar viewer look better and added a link that exports the calendar in the standard iCal format

--- deme_django/modules/event/views.py
+++ deme_django/modules/event/views.py
@@ -4,7 +4,9 @@ from cms.views import HtmlDocumentViewer, ItemViewer
 from cms.models import *
 from django.db.models import Q
 from modules.event.models import Event
-from datetime import date
+from datetime import date, datetime
+from icalendar import Calendar as iCal
+from icalendar import Event as iEvent
 import calendar
 import time
 
@@ -72,6 +74,7 @@ class CalendarViewer(ItemViewer):
                         events[this_day] = day_event_list
 
         event_week_list = []
+        actual_today = date.today()
 
         for week in week_list:
             event_week = []
@@ -81,6 +84,7 @@ class CalendarViewer(ItemViewer):
                 days_events["day"] = day
                 days_events["cur_events"] = events.get(day, [])
                 days_events["in_cur_month"] = (day.month == today.month)
+                days_events["is_today"] = (day == actual_today)
                 event_week.append(days_events)
 
             event_week_list.append(event_week)
@@ -90,5 +94,31 @@ class CalendarViewer(ItemViewer):
         self.context['next_month_offset'] = month_offset + 1
         self.context['prev_month_offset'] = month_offset - 1
         return HttpResponse(template.render(self.context))
+
+    def item_export_html(self):
+        cal = iCal()
+        collection = self.item
+
+        if self.cur_agent_can_global('do_anything'):
+            recursive_filter = None
+        else:
+            visible_memberships = self.permission_cache.filter_items('view Membership.item', Membership.objects)
+            recursive_filter = Q(child_memberships__in=visible_memberships.values('pk').query)
+        all_members = collection.all_contained_collection_members(recursive_filter).order_by('-created_at')
+
+        for member in all_members:
+            if issubclass(member.actual_item_type(), Event): 
+                member = member.downcast()
+                newEvent = iEvent()
+                newEvent.add('summary', member.display_name())
+                newEvent.add('dtstart', datetime.combine(member.start_date, member.start_time))
+                newEvent.add('dtend', datetime.combine(member.start_date, member.start_time))
+                newEvent.add('location', member.location)
+                newEvent.add('description', member.body)
+                cal.add_component(newEvent)
+
+        response = HttpResponse(cal.as_string(), mimetype='ics')
+        response['Content-Disposition'] = 'attachment; filename=demeCalendar.ics'
+        return response
 
 
