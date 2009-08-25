64f1e814f8707e74f2a254fcb25089304638e7a9
Joseph <joe@joe-laptop.(none)>
2009-08-24 22:11:57 -0700

    I messed up copying files to my laptop and i think I committed wrong; i reversed it and it should be right now.

--- deme_django/modules/symsys/views.py
+++ deme_django/modules/symsys/views.py
@@ -9,6 +9,17 @@ class SymsysCareerViewer(ItemViewer):
     accepted_item_type = SymsysCareer
     viewer_name = 'symsyscareer'
 
+    def item_show_html(self):
+        #from django.utils.safestring import mark_safe
+        self.context['action_title'] = ''
+        self.require_ability('view ', self.item, wildcard_suffix=True)
+        template = loader.get_template('symsyscareer/show.html')
+        if issubclass(self.item.actual_item_type(), ProgramStaffSymsysCareer):
+            self.item = self.item.downcast()
+            self.context['admin_title'] = self.item.admin_title
+        self.context['item'] = self.item
+        return HttpResponse(template.render(self.context))
+
 
 class ThesisSymsysCareerViewer(SymsysCareerViewer):
     accepted_item_type = ThesisSymsysCareer
@@ -60,6 +71,7 @@ class SymsysAffiliateViewer(PersonViewer):
     viewer_name = 'symsysaffiliate'
 
     def item_show_html(self):
+        from django.utils.safestring import mark_safe
         self.context['action_title'] = ''
         self.require_ability('view ', self.item, wildcard_suffix=True)
         template = loader.get_template('symsysaffiliate/show.html')
@@ -69,7 +81,35 @@ class SymsysAffiliateViewer(PersonViewer):
             visible_memberships = self.permission_cache.filter_items('view Membership.item', Membership.objects)
             recursive_filter = Q(child_memberships__in=visible_memberships.values('pk').query)
         self.context['containing_collections'] = self.item.ancestor_collections(recursive_filter)
-        self.context['contact_methods'] = self.permission_cache.filter_items('view ContactMethod.agent', self.item.contact_methods).filter(active=True)
+    
+        contact_method_fields = []
+        contact_methods = self.permission_cache.filter_items('view ContactMethod.agent', self.item.contact_methods).filter(active=True)
+        for contact_method in contact_methods:
+            if issubclass(contact_method.actual_item_type(), EmailContactMethod):
+                contact_method = contact_method.downcast()
+                contact_method_fields.append(contact_method.email)
+            if issubclass(contact_method.actual_item_type(), WebsiteContactMethod):
+                contact_method = contact_method.downcast()
+                link = ("""<a href="%s">%s</a>""" % (contact_method.url, contact_method.url))
+                contact_method_fields.append(mark_safe(link))
+            if issubclass(contact_method.actual_item_type(), PhoneContactMethod):
+                contact_method = contact_method.downcast()
+                contact_method_fields.append(contact_method.phone)
+            if issubclass(contact_method.actual_item_type(), FaxContactMethod):
+                contact_method = contact_method.downcast()
+                contact_method_fields.append('(Fax) ' + contact_method.fax)
+            if issubclass(contact_method.actual_item_type(), AIMContactMethod):
+                contact_method = contact_method.downcast()
+                contact_method_fields.append(contact_method.screen_name)
+            if issubclass(contact_method.actual_item_type(), AddressContactMethod):
+                contact_method = contact_method.downcast()
+                if contact_method.street2:
+                    address = (""" %s <br> %s <br>%s, %s %s  %s """ % (contact_method.street1, contact_method.street2, contact_method.city, contact_method.state, contact_method.country, contact_method.zip))
+                else:
+                    address = (""" %s <br>%s, %s %s  %s """ % (contact_method.street1, contact_method.city, contact_method.state, contact_method.country, contact_method.zip))
+                contact_method_fields.append(mark_safe(address))
+
+        self.context['contact_methods'] = contact_method_fields
         self.context['symsys_careers'] = self.permission_cache.filter_items('view SymsysCareer.symsys_affiliate', self.item.symsys_careers).filter(active=True)
         return HttpResponse(template.render(self.context))
 
