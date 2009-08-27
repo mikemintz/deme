783b037e87a7a4ad20cc39adef2e3a912e2f99dc
Joseph <joe@joe-laptop.(none)>
2009-08-27 15:06:11 -0700

    Fixed a bug where it would raise an error if there was an embedded item with no query string

--- deme_django/cms/base_viewer.py
+++ deme_django/cms/base_viewer.py
@@ -317,7 +317,8 @@ class Viewer(object):
         else:
             path = reverse('item_url', kwargs={'viewer': self.viewer_name, 'action': action, 'noun': item.pk})
         if not query_string:
-            query_string = original_viewer.context['full_path'].split('?', 1)[1]
+            if original_viewer.context['full_path'].find('?') != -1: 
+                query_string = original_viewer.context['full_path'].split('?', 1)[1]
         self.request = VirtualRequest(original_viewer.request, path, query_string)
         self.method = self.request.method
         self.cur_agent = original_viewer.cur_agent
