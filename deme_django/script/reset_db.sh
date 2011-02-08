f2ffe16583e92b416edaf533cc5eff62b14ae603
jmarrama <jmarrama@joe-desktop.(none)>
2011-02-08 00:16:31 -0800

    fixed reset_db to not be annoying with south

--- deme_django/script/reset_db.sh
+++ deme_django/script/reset_db.sh
@@ -6,5 +6,6 @@ rm -rf "$base_dir/static/filedocument/*"
 psql -U postgres -c "drop database deme_django;"
 psql -U postgres -c "create database deme_django WITH OWNER = postgres ENCODING = 'UTF8' TABLESPACE = pg_default;"
 $base_dir/manage.py syncdb --all
-$base_dir/manage.py migrate
-$base_dir/script/create_initial_data.py test
+$base_dir/manage.py migrate --fake
+#$base_dir/script/create_initial_data.py test
+$base_dir/script/create_initial_data.py 
