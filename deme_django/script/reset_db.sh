51bf8fd762791b1b931dc5096649c66780b50965
jmarrama <jmarrama@stanford.edu>
2011-05-13 11:27:55 -0700

    quick test

--- deme_django/script/reset_db.sh
+++ deme_django/script/reset_db.sh
@@ -7,5 +7,5 @@ psql -U postgres -c "drop database deme_django;"
 psql -U postgres -c "create database deme_django WITH OWNER = postgres ENCODING = 'UTF8' TABLESPACE = pg_default;"
 $base_dir/manage.py syncdb --all
 $base_dir/manage.py migrate --fake
-#$base_dir/script/create_initial_data.py test
-$base_dir/script/create_initial_data.py 
+$base_dir/script/create_initial_data.py test
+#$base_dir/script/create_initial_data.py 
