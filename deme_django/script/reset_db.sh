dafe3f73104bd8374c658ae2904b9624d4cae879
Mike Mintz <mikemintz@cs.stanford.edu>
2011-06-04 13:42:03 -0700

    Don't throw an error if deme_django database does not exist in script/reset_db.sh

--- deme_django/script/reset_db.sh
+++ deme_django/script/reset_db.sh
@@ -3,8 +3,8 @@
 cur_dir=`dirname $0`
 base_dir="$cur_dir/.."
 rm -rf "$base_dir/static/filedocument/*"
-psql -U postgres -c "drop database deme_django;"
-psql -U postgres -c "create database deme_django WITH OWNER = postgres ENCODING = 'UTF8' TABLESPACE = pg_default;"
+psql -U postgres -c "DROP DATABASE IF EXISTS deme_django;"
+psql -U postgres -c "CREATE DATABASE deme_django WITH OWNER = postgres ENCODING = 'UTF8' TABLESPACE = pg_default;"
 $base_dir/manage.py syncdb --all
 $base_dir/manage.py migrate --fake
 $base_dir/script/create_initial_data.py test
