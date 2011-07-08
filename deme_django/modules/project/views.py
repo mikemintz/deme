45b7301f99d855c2cee6095e26f94a2c8238ce74
Jimmy Tobin <jtobin1@stanford.edu>
2011-07-08 14:35:56 -0700

    Project Viewer has due date and collection

--- deme_django/modules/project/views.py
+++ deme_django/modules/project/views.py
@@ -16,12 +16,18 @@ class ProjectViewer(CollectionViewer):
 
 
     def item_show_html(self):
-        self.context['action_title'] = 'Show'
-
-        self.context['project'] = self.item
-
-
-
+        from cms.forms import AjaxModelChoiceField
+        self.context['action_title'] = ''
+        self.require_ability('view ', self.item, wildcard_suffix=True)
+        memberships = self.item.child_memberships
+        memberships = memberships.filter(active=True)
+        memberships = memberships.filter(item__active=True)
+        memberships = self.permission_cache.filter_items('view Membership.item', memberships)
+        memberships = memberships.select_related('item')
+        if memberships:
+            self.permission_cache.filter_items('view Item.name', Item.objects.filter(pk__in=[x.item_id for x in memberships]))
+        self.context['memberships'] = sorted(memberships, key=lambda x: (not self.permission_cache.agent_can('view Item.name', x.item), x.item.name))
+        self.context['cur_agent_in_collection'] = bool(self.item.child_memberships.filter(active=True, item=self.cur_agent))
         template = loader.get_template('project/show.html')
         return HttpResponse(template.render(self.context))
         
