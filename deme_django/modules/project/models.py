8de43e19b6996b0716bcf8090ccc6f0ab187d95e
Mike Mintz <mikemintz@cs.stanford.edu>
2011-07-15 10:58:13 -0800

    Project module: renamed beggining_time to beginning_time and added initial migration

--- deme_django/modules/project/models.py
+++ deme_django/modules/project/models.py
@@ -23,14 +23,14 @@ class Project(Collection):
         verbose_name_plural = _('projects')
 
     #fields:
-    due_date = models.DateField(_('due date'), help_text=_('Dates must be entered in the format "MM/DD/YY"'))
-    due_time = models.TimeField(_('due time'))
+    due_date = models.DateField(_('due date'), null=True, blank=True, default=None, help_text=_('Dates must be entered in the format "MM/DD/YY"'))
+    due_time = models.TimeField(_('due time'), null=True, blank=True, default=None)
     
 class Task(HtmlDocument):
 
     #Setup
     introduced_immutable_fields = frozenset()
-    introduced_abilities = frozenset(['view Task.due_date', 'edit Task.due_date', 'view Task.beggining_date', 'edit Task.beggining_date',
+    introduced_abilities = frozenset(['view Task.due_date', 'edit Task.due_date', 'view Task.beginning_date', 'edit Task.beginning_date',
                                      'view Task.due_time', 'edit Task.due_time', 'view Task.priority', 'edit Task.priority',
                                      'view Task.length', 'edit Task.length', 'view Task.status', 'edit Task.status',
                                      'view Task.is_repeating', 'edit Task.is_repeating', 'view Task.task_handler', 'edit Task.task_handler'])
@@ -42,26 +42,26 @@ class Task(HtmlDocument):
         verbose_name_plural = _('tasks')
 
     #fields:
-    due_date = models.DateField(_('due date'), help_text=_('Dates must be entered in the format "MM/DD/YY"'))
-    beggining_date = models.DateField(_('beggining date'), help_text=_('Dates must be entered in the format "MM/DD/YY"'))
+    due_date = models.DateField(_('due date'), null=True, blank=True, default=None, help_text=_('Dates must be entered in the format "MM/DD/YY"'))
+    beginning_date = models.DateField(_('beginning date'), null=True, blank=True, default=None, help_text=_('Dates must be entered in the format "MM/DD/YY"'))
     
-    due_time = models.TimeField(_('due time'))
+    due_time = models.TimeField(_('due time'), null=True, blank=True, default=None)
     
     Priority_Choices = (
         ('Low', 'Low'),
         ('Medium', 'Medium'),
         ('High', 'High'),
     )
-    priority = models.CharField(_('priority'),max_length=10, choices=Priority_Choices)
+    priority = models.CharField(_('priority'), max_length=10, default='Low', choices=Priority_Choices)
     
-    length = models.DecimalField(_('length'), max_digits=6, decimal_places = 2, help_text=_('in hours'))
+    length = models.DecimalField(_('length'), default=0, max_digits=6, decimal_places=2, help_text=_('in hours'))
     
     Status_Choices = (
         ('Unassigned', 'Unassigned'),
         ('Assigned', 'Assigned'),
         ('Completed', 'Completed'),
     )
-    status = models.CharField(_('status'),max_length=16, choices=Status_Choices)
+    status = models.CharField(_('status'), default='Unassigned', max_length=16, choices=Status_Choices)
 	
     is_repeating = FixedBooleanField(_('is repeating'), default=False, help_text=_('Select this if this task will be repeated.'))
     
