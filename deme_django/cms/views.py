9cd43b116b0e7a482bbb3cf1c16df57f3b386ec9
Mike Mintz <mikemintz@cs.stanford.edu>
2011-09-29 13:01:02 -0700

    Don't display current page in recently viewed, move box above navigatoin box

--- deme_django/cms/views.py
+++ deme_django/cms/views.py
@@ -747,7 +747,7 @@ class ItemViewer(Viewer):
         if clear_history:
             pages = []
             self.request.session['recentlyviewed'] = pages
-        data = pages[:num_pages]
+        data = pages[1:num_pages]
         json_str = simplejson.dumps(data, separators=(',',':'))
         return HttpResponse(json_str, mimetype='application/json')
 
