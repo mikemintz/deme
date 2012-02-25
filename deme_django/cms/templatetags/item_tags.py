673b771a48d6ee5f29a3555ba6896f4cab016af6
Mike Mintz <mikemintz@cs.stanford.edu>
2012-02-25 11:31:35 -0800

    Replaced blanks in menus with ------

--- deme_django/cms/templatetags/item_tags.py
+++ deme_django/cms/templatetags/item_tags.py
@@ -507,7 +507,7 @@ class ActionsMenu(template.Node):
             """ % destroy_url)
 
         result.append("""
-        <a href="#" class="fg-button fg-button-icon-right ui-widget ui-state-default ui-corner-all" id="actions_menu_link"><span class="ui-icon ui-icon-triangle-1-s"></span>&nbsp;</a>
+        <a href="#" class="fg-button fg-button-icon-right ui-widget ui-state-default ui-corner-all" id="actions_menu_link"><span class="ui-icon ui-icon-triangle-1-s"></span>------</a>
         <div style="display: none;">
             <ul style="font-size: 85%;">
         """)
