43b01ede18609c13093e68c4389546d984755139
Mike Mintz <mikemintz@cs.stanford.edu>
2012-08-02 19:07:00 -0700

    Approvenpoll should be more compatible with python 2.7

--- deme_django/modules/poll/views.py
+++ deme_django/modules/poll/views.py
@@ -154,10 +154,10 @@ class ApproveNPollViewer(PollViewer):
             #maxTemp = max([agree, disagree, no_vote])
             no_vote = n-agree-disagree
             #if(maxTemp > maxNumber): maxNumber = maxTemp
-            vote_numbers.append({'agree': int(agree), 'n':range(n), 'agreeList':range(agree), 'disagree': int(disagree), 'disagreeList': range(disagree), 'no_vote':int(no_vote), 'no_voteList':range(no_vote), 'proposition': proposition})
+            vote_numbers.append({'agree': int(agree), 'n':range(int(n)), 'agreeList':range(agree), 'disagree': int(disagree), 'disagreeList': range(disagree), 'no_vote':int(no_vote), 'no_voteList':range(no_vote), 'proposition': proposition})
         
         #self.context['maxVal'] = maxNumber
-        self.context['rangeN'] = range(n+1)
+        self.context['rangeN'] = range(int(n)+1)
         self.context['proportion'] = int(12.0*15.0/n)
         self.context['2proportion'] = 2*int(12.0*15.0/n)
         self.context['vote_numbers_list'] = vote_numbers
