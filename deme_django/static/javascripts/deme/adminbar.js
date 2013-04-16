97159034660363c970177d1ec03c373b7b13714e
Michael Huang <mikhuang@gmail.com>
2013-04-15 22:27:35 -0700

    put search box into menu

--- deme_django/static/javascripts/deme/adminbar.js
+++ deme_django/static/javascripts/deme/adminbar.js
@@ -1,2 +1,13 @@
 $(function(){
+  // if actions menu is empty, remove it
+  if ($.trim($('#actions-btn .actions-menu').text()) == '') {
+    $('#actions-btn').remove();
+  }
+
+  // attach focus to search button
+  $('#search-btn a').click(function(){
+    setTimeout(function(){
+      $('#search_box').focus();
+    }, 1);
+  });
 });
