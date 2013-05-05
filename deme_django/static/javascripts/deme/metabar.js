38c5ebe71637d2bc2c72955d8d7227b5e92e68ca
Michael Huang <mikhuang@gmail.com>
2013-05-05 11:14:32 -0700

    added buttons to resize metadata area

--- deme_django/static/javascripts/deme/metabar.js
+++ deme_django/static/javascripts/deme/metabar.js
@@ -1,8 +1,10 @@
 $.cookie.defaults.path = '/';
 
 $(function(){
+  var METABAR_RESIZE_STEP = 50;
   var $metabar = $('#metabar');
-  var metabar_width = 200;
+  var metabar_width = min_metabar_width = 200; // minimum/default width
+  var max_metabar_width = 800;
 
   // sets metabar sizing styles based on window dimensions
   function metabar_sizing(){
@@ -13,7 +15,8 @@ $(function(){
 
   // attach resizable to entire metabar
   $metabar.resizable({
-    minWidth: metabar_width,
+    minWidth: min_metabar_width,
+    maxWidth: max_metabar_width,
     handles: 'w',
     start: function() {
       $metabar.addClass('noanim');
@@ -21,17 +24,36 @@ $(function(){
     stop: function(ev, ui) {
       $metabar.removeClass('noanim');
       var width = ui.size.width;
-      metabar_width = width;
-      // save cookie
-      $.cookie('METABAR_WIDTH', width);
-      metabarWidthAdjust(metabar_width);
+      setMetabarWidth(width);
     }
   });
 
+  // setter for width
+  function setMetabarWidth(width) {
+    if (width < min_metabar_width) {
+      width = min_metabar_width;
+    } else if (width > max_metabar_width) {
+      width = max_metabar_width;
+    }
+    // save cookie
+    $.cookie('METABAR_WIDTH', width);
+    metabarWidthAdjust(width);
+    metabar_width = width;
+  }
+
+  // attach events to width changing buttons
+  $('#metabar').on('click', 'button.resize', function(e){
+    var new_width = metabar_width * 1.0 - METABAR_RESIZE_STEP;
+    if ($(this).hasClass('resize-left')) {
+      new_width = metabar_width * 1.0 + METABAR_RESIZE_STEP;
+    }
+    setMetabarWidth(new_width);
+  });
+
+  // load metabar width from cookie
   var past_width = $.cookie('METABAR_WIDTH');
   if (past_width) {
-    metabar_width = past_width;
-    $metabar.css('width', past_width);
+    setMetabarWidth(past_width);
   }
 
   // show/hide meta bar
@@ -55,15 +77,16 @@ $(function(){
 
   // adjust width of container to take into account metadata width
   function metabarWidthAdjust(metabar_width) {
-    var width = '';
+    var page_layout_width = '';
     if (typeof(metabar_width) == 'undefined') {
       metabar_width = $metabar.width();
     }
     // if visible, then calculate
     if (!$metabar.hasClass('closed')) {
-      width = $(window).width() - metabar_width - 10 + 'px'; // buffer
+      page_layout_width = $(window).width() - metabar_width - 10 + 'px'; // buffer
     }
-    $('.page-layout').css('max-width',width)
+    $metabar.css('width', metabar_width);
+    $('.page-layout').css('max-width', page_layout_width)
   }
 
   // attach event to adminbar
