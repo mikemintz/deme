78b8785c7938267315e348c1aefeef35da54e20e
Michael Huang <mikhuang@gmail.com>
2013-05-04 21:02:24 -0700

    changing person profile to be targeted at all agents, copied over contact method display from symsys/views.py

--- deme_django/cms/views.py
+++ deme_django/cms/views.py
@@ -830,6 +830,47 @@ class AgentViewer(ItemViewer):
     accepted_item_type = Agent
     viewer_name = 'agent'
 
+    def item_show_html(self):
+        self.context['action_title'] = ''
+        self.require_ability('view ', self.item, wildcard_suffix=True)
+        # copied from symsys/views.py
+        contact_method_fields = []
+        contact_methods = self.permission_cache.filter_items('view ContactMethod.agent', self.item.contact_methods).filter(active=True)
+        for contact_method in contact_methods:
+            if issubclass(contact_method.actual_item_type(), EmailContactMethod):
+                contact_method = contact_method.downcast()
+                if self.permission_cache.agent_can('view EmailContactMethod.email', contact_method):
+                    contact_method_fields.append(contact_method.email)
+            if issubclass(contact_method.actual_item_type(), WebsiteContactMethod):
+                contact_method = contact_method.downcast()
+                if self.permission_cache.agent_can('view WebsiteContactMethod.url', contact_method):
+                    link = ("""<a href="%s">%s</a>""" % (contact_method.url, contact_method.url))
+                    contact_method_fields.append(mark_safe(link))
+            if issubclass(contact_method.actual_item_type(), PhoneContactMethod):
+                contact_method = contact_method.downcast()
+                if self.permission_cache.agent_can('view PhoneContactMethod.phone', contact_method):
+                    contact_method_fields.append(contact_method.phone)
+            if issubclass(contact_method.actual_item_type(), FaxContactMethod):
+                contact_method = contact_method.downcast()
+                if self.permission_cache.agent_can('view FaxContactMethod.fax', contact_method):
+                    contact_method_fields.append('(Fax) ' + contact_method.fax)
+            if issubclass(contact_method.actual_item_type(), AIMContactMethod):
+                contact_method = contact_method.downcast()
+                if self.permission_cache.agent_can('view AIMContactMethod.screen_name', contact_method):
+                    contact_method_fields.append('(AIM Screename) ' + contact_method.screen_name)
+            if issubclass(contact_method.actual_item_type(), AddressContactMethod):
+                contact_method = contact_method.downcast()
+                if self.permission_cache.agent_can('view AddressContactMethod.street1', contact_method):
+                    if contact_method.street2:
+                        address = (""" %s <br> %s <br>%s, %s %s  %s """ % (contact_method.street1, contact_method.street2, contact_method.city, contact_method.state, contact_method.country, contact_method.zip))
+                    else:
+                        address = (""" %s <br>%s, %s %s  %s """ % (contact_method.street1, contact_method.city, contact_method.state, contact_method.country, contact_method.zip))
+                    contact_method_fields.append(mark_safe(address))
+
+        self.context['contact_methods'] = contact_method_fields
+
+        template = loader.get_template('agent/show.html')
+        return HttpResponse(template.render(self.context))
 
 class AnonymousAgentViewer(AgentViewer):
     accepted_item_type = AnonymousAgent
@@ -910,13 +951,6 @@ class AuthenticationMethodViewer(ItemViewer):
 class PersonViewer(AgentViewer):
     accepted_item_type = Person
     viewer_name = 'person'
-
-    def item_show_html(self):
-        self.context['action_title'] = ''
-        self.require_ability('view ', self.item, wildcard_suffix=True)
-
-        template = loader.get_template('person/show.html')
-        return HttpResponse(template.render(self.context))
 
 
 class ContactMethodViewer(ItemViewer):
