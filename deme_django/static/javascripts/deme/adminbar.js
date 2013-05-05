5c033bf7ad634a83fc076286eef132261de1bf50
Michael Huang <mikhuang@gmail.com>
2013-05-05 12:27:03 -0700

    text size button issues - option to reset to default, default list of possible font sizes

--- deme_django/static/javascripts/deme/adminbar.js
+++ deme_django/static/javascripts/deme/adminbar.js
@@ -13,30 +13,57 @@ $(function(){
     }, 1);
   });
 
+  var defaultFontSize = 14;
+  var validFontSizes = [8,9,10,12,14,16,18,20,24,30];
+
   // user set text size
-  function resizeText(delta) {
-    // TODO: have a list of preset font sizes, probably something like 10 of them. circulate amongst those.
+  function resizeTextDelta(delta) {
+    // have a list of preset font sizes, probably something like 10 of them. circulate amongst those.
     if (document.body.style.fontSize == "") {
-      document.body.style.fontSize = "14px";
+      document.body.style.fontSize = defaultFontSize + "px";
     }
-    var size = parseFloat(document.body.style.fontSize) + (delta) + "px";
-    document.body.style.fontSize = size;
+    current_size = parseFloat(document.body.style.fontSize);
+    pos = validFontSizes.indexOf(current_size);
+    default_pos = validFontSizes.indexOf(defaultFontSize);
+    if (pos < 0 || delta == 0) {
+      pos = default_pos;
+    }
+    pos = pos + delta;
+    if (pos <= 0) {
+      pos = 0;
+    }
+    if (pos >= validFontSizes.length) {
+      pos = validFontSizes.length - 1;
+    }
+    var size = validFontSizes[pos] + 'px';
     $.cookie('ADMINBAR_TEXTSIZE_SIZE', size);
+    resizeText(size);
   }
+
+  function resizeText(size) {
+    $('#textsize-btn').addClass('resized');
+    if (parseFloat(size) == defaultFontSize) {
+      $('#textsize-btn').removeClass('resized');
+    }
+    document.body.style.fontSize = size;
+  }
+
   window.resizeText = resizeText;
   // load font size
   var size = $.cookie('ADMINBAR_TEXTSIZE_SIZE');
   if (size) {
-    document.body.style.fontSize = size;
+    resizeText(size);
   }
   $('#textsize-btn .btn').click(function(e){
     e.preventDefault();
     e.stopPropagation();
-    var delta = 1;
+    var delta = 0;
     if ($(this).hasClass('smaller')) {
       delta = -1;
+    } else if ($(this).hasClass('bigger')) {
+      delta = 1;
     }
-    resizeText(delta);
+    resizeTextDelta(delta);
   });
 
   // set up create, edit, and comment buttons based on existing markup elsewhere
