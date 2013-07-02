f7535653598943c27f21f125e11b4eea18822e0b
Mike Mintz <mikemintz@cs.stanford.edu>
2013-07-02 09:13:36 -0700

    Added add_agents_from_csv script.

---
+++ deme_django/script/add_agents_from_csv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from django.core.management import setup_environ
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from deme_django import settings
+setup_environ(settings)
+
+from cms.models import *
+from modules.demeaccount.models import *
+filename = sys.argv[1]
+data = [x.strip().split(',') for x in open(filename)][1:]
+admin = Agent.objects.get(pk=1)
+for name, password, group_name in data:
+    agent = Agent(name=name)
+    agent.save_versioned(admin)
+    deme_account = DemeAccount(username=name, agent=agent)
+    deme_account.set_password(password)
+    deme_account.save_versioned(admin)
+    group = Group.objects.get(name=group_name)
+    Membership(item=agent, collection=group).save_versioned(admin)
