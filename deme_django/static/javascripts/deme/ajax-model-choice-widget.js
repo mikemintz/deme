c78d9a2247c3136815a7e38b0f3ff168ccd69e1a
Michael Huang <mikhuang@gmail.com>
2013-07-03 10:00:16 -0700

    Need to add back button functionality to Firefox and IE

--- deme_django/static/javascripts/deme/ajax-model-choice-widget.js
+++ deme_django/static/javascripts/deme/ajax-model-choice-widget.js
@@ -43,7 +43,7 @@ $(function() {
       var list_modal_url = hidden_input.attr('data-list-modal-url');
 
       var random_num = Math.floor((Math.random()*1000000)+1);
-      window.open(list_modal_url, 'embedform' + random_num, 'width=600,toolbar=1,resizable=1,scrollbars=yes,height=600,top=100,left=100');
+      window.open(list_modal_url, 'embedform' + random_num, 'width=600,toolbar=1,location=yes,resizable=1,scrollbars=yes,height=600,top=100,left=100');
     });
 
     hidden_input.addClass('ajax-model-choice-widget-processed');
